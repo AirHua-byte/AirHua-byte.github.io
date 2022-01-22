cbaf691b0e92e084b8621afedb03d86360203791
AirHua-byte <3301833942@qq.com>
2022-01-22 20:50:12 +0800

    deploy

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 如果是发布到自定义域名
+# echo 'www.yourwebsite.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果你想要部署到 https://USERNAME.github.io
+git push -f git@github.com:AirHua-byte/AirHua-byte.github.io.git main
+
+# 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
+# git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
+
+cd -
