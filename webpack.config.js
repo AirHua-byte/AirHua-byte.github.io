3ab85d1e3cdbd247cd56833ae122a0a4e694e8b9
AirHua-byte <3301833942@qq.com>
2022-01-22 17:45:38 +0800

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     app: ['@babel/polyfill', './src/app.js']
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'app.bundle.js'
   },
   module: {
